8e06facb0cfe876b48b46a763a9f17b72ca977b9
somto idafum <idafum@example.com>
2024-12-07 18:44:32 -0600

    setup httpClient, add gitignore

--- UserProfilesConsoleApp/Program.cs
+++ UserProfilesConsoleApp/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+
+namespace UserProfilesConsoleApp
+{
+    class Program
+    {
+        private static readonly HttpClient client;
+
+        //Static Constructor
+        static Program()
+        {
+            var cookieContainer = new CookieContainer();
+
+            //This handler will establish a new connection to the server after 30 min
+            //This will respect DNS changes and manage resources
+            var handler = new SocketsHttpHandler
+            {
+                UseCookies = true,
+                CookieContainer = cookieContainer,
+                PooledConnectionLifetime = TimeSpan.FromMinutes(30)
+            };
+
+            client = new HttpClient(handler)
+            {
+                BaseAddress = new Uri("http://api.combatcritters.ca:4000")
+            };
+        }
+
+        static async Task Main(string[] args)
+        {
+
+        }
+    }
+}
